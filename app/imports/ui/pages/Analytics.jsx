a2c8cea82f04c98b61730fc4e2712361d21c76b4
James Ligeralde <jligeral@hawaii.edu>
2023-11-23 12:45:27 -1000

    Updated graph-data method and analytics page

--- app/imports/ui/pages/Analytics.jsx
+++ app/imports/ui/pages/Analytics.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row, Card } from 'react-bootstrap';
 import { Doughnut } from 'react-chartjs-2';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend, Colors } from 'chart.js';
 
-// The ChartJS library requires that the following lines be included in order to render the chart
+// The ChartJS library requires that the following lines be included to render the chart
 ChartJS.register(ArcElement, Tooltip, Legend, Colors);
 
 const Analytics = () => {
@@ -27,18 +27,14 @@ const Analytics = () => {
     // Call the fetchGraphData function when the component is first rendered
     fetchGraphData();
   }, []); // The empty dependency array ensures that this effect runs only once, similar to componentDidMount
+
   // The data object is used to configure the chart
   const data = {
-    // The labels array contains the labels for each section of the chart
-    labels: articleLabels,
-    // The datasets array contains the data for each section of the chart
+    labels: articleLabels.map((label) => label.label),
     datasets: [
       {
-        // The label property is used to display the label for the dataset
         label: '# of Uses',
-        // The data property is used to set the data for the dataset
         data: articleData,
-        // The borderWidth property is used to set the width of the border around each section of the chart
         borderWidth: 1,
       },
     ],
@@ -47,7 +43,8 @@ const Analytics = () => {
   return (
     <Container className="py-3">
       <Row className="justify-content-center">
-        <Col md={7}>
+        {/* Left column for Doughnut chart */}
+        <Col md={7} className="border-right">
           <Col className="text-center">
             <h2>Analytics</h2>
           </Col>
@@ -58,6 +55,26 @@ const Analytics = () => {
               <h3>Currently no user data</h3>
             )}
           </Col>
+        </Col>
+
+        {/* Right column for the list of top three sources */}
+        <Col md={5} className="align-items-center">
+          <Col className="text-center">
+            <h2>Top Used Articles</h2>
+          </Col>
+          {articleLabels.slice(0, 3).map((label, index) => (
+            <Card key={index} className="my-2">
+              <Card.Body>
+                <Card.Title>{label.label}</Card.Title>
+                <Card.Text>
+                  {/* Render the link */}
+                  <a href={label.link} target="_blank" rel="noopener noreferrer">
+                    {label.link}
+                  </a>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          ))}
         </Col>
       </Row>
     </Container>

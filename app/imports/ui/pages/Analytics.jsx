09d9f576143ee54b39fad5a5050040a444991a75
jsapolu99 <109643051+jsapolu99@users.noreply.github.com>
2023-11-23 00:20:50 -1000

    Updated Analytics Page
    - Limited analytics graph to the top 6 most frequently used articles.
    
    - Added comments explaining code.

--- app/imports/ui/pages/Analytics.jsx
+++ app/imports/ui/pages/Analytics.jsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Col, Container, Row } from 'react-bootstrap';
 import { Doughnut } from 'react-chartjs-2';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-//  { Articles } from '../../api/articles/Articles';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, Colors } from 'chart.js';
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+// The ChartJS library requires that the following lines be included in order to render the chart
+ChartJS.register(ArcElement, Tooltip, Legend, Colors);
 
 const Analytics = () => {
   const [articleLabels, setArticleLabels] = useState([]);
@@ -15,39 +15,30 @@ const Analytics = () => {
     const fetchGraphData = () => {
       Meteor.call('getGraphData', (error, response) => {
         if (!error) {
+          // Set the articleLabels and articleData state variables to the values returned by the getGraphData method
           setArticleLabels(response.articleLabels);
           setArticleData(response.articleData);
         } else {
+          // eslint-disable-next-line no-console
           console.error('Error fetching graph data:', error);
         }
       });
     };
-
+    // Call the fetchGraphData function when the component is first rendered
     fetchGraphData();
   }, []); // The empty dependency array ensures that this effect runs only once, similar to componentDidMount
-
+  // The data object is used to configure the chart
   const data = {
+    // The labels array contains the labels for each section of the chart
     labels: articleLabels,
+    // The datasets array contains the data for each section of the chart
     datasets: [
       {
+        // The label property is used to display the label for the dataset
         label: '# of Uses',
+        // The data property is used to set the data for the dataset
         data: articleData,
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
+        // The borderWidth property is used to set the width of the border around each section of the chart
         borderWidth: 1,
       },
     ],
@@ -60,11 +51,11 @@ const Analytics = () => {
           <Col className="text-center">
             <h2>Analytics</h2>
           </Col>
-          <Col>
+          <Col className="text-center">
             {articleLabels.length > 0 ? (
               <Doughnut data={data} />
             ) : (
-              <p>Loading...</p>
+              <h3>No data to display</h3>
             )}
           </Col>
         </Col>
--- app/server/graph-data.js
+++ app/server/graph-data.js
@@ -3,9 +3,12 @@ import { Articles } from '../imports/api/articles/Articles';
 
 Meteor.methods({
   'getGraphData'() {
-    const articles = Articles.collection.find({ useCount: { $gt: 0 } }).fetch();
+    // Get the top 6 articles with the most useCount greater than 0
+    const articles = Articles.collection.find({ useCount: { $gt: 0 } }, { sort: { useCount: -1 }, limit: 6 }).fetch();
+    // Get useCount and title for each article
     const articleData = articles.map((article) => article.useCount);
     const articleLabels = articles.map((article) => article.title);
+    // Return articleData and articleLabels
     return {
       articleData: articleData,
       articleLabels: articleLabels,

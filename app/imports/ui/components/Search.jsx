8c854884b2db23b16df61a6b81c2c5c5019860be
James Ligeralde <jligeral@hawaii.edu>
2023-12-04 08:13:46 -1000

    Creates new search bar after latest message

--- app/imports/ui/components/Search.jsx
+++ app/imports/ui/components/Search.jsx
@@ -7,6 +7,8 @@ import LoadingSpinner from './LoadingSpinner';
 
 const ITSearch = () => {
   const [userInput, setUserInput] = useState('');
+  const [inputDisabled, setInputDisabled] = useState(false);
+  const [showNewSearchBar, setShowNewSearchBar] = useState(false);
   const [chatMessages, setChatMessages] = useState([
     { role: 'assistant', content: 'Hello and welcome to Ask Us! How can I assist you today?', sources: null, titles: null, scores: null, isLoading: false },
   ]);
@@ -14,54 +16,49 @@ const ITSearch = () => {
   const handleUserInput = (e) => {
     setUserInput(e.target.value);
   };
-  // Call the useEffect hook to set up a listener on the chatMessages array
+
   useEffect(() => {
-    // Scroll to the bottom of the page when chatMessages change
     window.scrollTo(0, document.body.scrollHeight);
-  }, [chatMessages]); // Add chatMessages as a dependency
+  }, [chatMessages]);
 
   const handleSendMessage = () => {
-    // If the user did not enter any text, do nothing
     if (userInput.trim() === '') {
       return;
     }
-    // Add the user's message to the chatMessages array
+
     setChatMessages((prevChatMessages) => [
       ...prevChatMessages,
       { role: 'user', content: userInput, isLoading: true },
     ]);
-    // Get the current user's ID
+
+    setInputDisabled(true);
+    setUserInput('');
+
     let currentUser = Meteor.userId();
-    // If the user is not logged in, set their ID to 'anonymous'
     if (currentUser === null) {
       currentUser = 'anonymous';
     }
-    // Send the user's message and session ID to the server using the getChatResponse method
+
     Meteor.call('getChatResponse', currentUser, userInput, (error, response) => {
       if (!error) {
-        // Add the response from OpenAI to the chatMessages array
         setChatMessages(prevChatMessages => [
-          ...prevChatMessages.slice(0, -1), // All messages except the last one (being rendered)
-          { ...prevChatMessages[prevChatMessages.length - 1], isLoading: false }, // Ensure last message is no longer loading
+          ...prevChatMessages.slice(0, -1),
+          { ...prevChatMessages[prevChatMessages.length - 1], isLoading: false },
           { role: 'assistant', content: response.chatResponse, sources: response.linkArray, titles: response.titleArray, scores: response.scoreArray, isLoading: false },
         ]);
 
-        // Scroll to the bottom of the page
-        window.scrollTo(0, document.body.scrollHeight);
+        setInputDisabled(false);
+        setShowNewSearchBar(true);
       } else {
         console.error(error);
       }
     });
-
-    // Clear the search bar
-    setUserInput('');
   };
 
   const handleKeyDown = (e) => {
-    // Check if the pressed key is Enter
     if (e.key === 'Enter') {
-      e.preventDefault(); // Prevent the default behavior of the enter key (e.g., form submission)
-      handleSendMessage(); // Call your sendMessage function
+      e.preventDefault();
+      handleSendMessage();
     }
   };
 
@@ -90,6 +87,7 @@ const ITSearch = () => {
               value={userInput}
               onChange={handleUserInput}
               onKeyDown={handleKeyDown}
+              disabled={inputDisabled}
             />
             <InputGroup.Text id="btn-group">
               <Button onClick={handleSendMessage} id="query-submit" variant="light"><Search /></Button>
@@ -98,9 +96,32 @@ const ITSearch = () => {
         </Col>
         <Col xs={8} className="d-flex flex-column justify-content-start pt-3">
           {chatMessages.map((chat, index) => (
-            chat.isLoading ?
-              <LoadingSpinner key={index} /> :
-              <ChatItem content={chat.content} role={chat.role} sources={chat.sources} titles={chat.titles} scores={chat.scores} key={index} />
+            chat.isLoading ? (
+              <LoadingSpinner key={index} />
+            ) : (
+              <React.Fragment key={index}>
+                <ChatItem content={chat.content} role={chat.role} sources={chat.sources} titles={chat.titles} scores={chat.scores} key={index} />
+                {showNewSearchBar && index === chatMessages.length - 1 && (
+                  <InputGroup className="mb-3 search-bar-input-group py-3">
+                    <Form.Control
+                      id={`new-search-bar-${index}`}
+                      placeholder="Ask another question..."
+                      aria-label="Recipient's username"
+                      aria-describedby="basic-addon2"
+                      value={userInput}
+                      onChange={handleUserInput}
+                      onKeyDown={handleKeyDown}
+                      disabled={inputDisabled}
+                    />
+                    <InputGroup.Text id={`btn-group-${index}`}>
+                      <Button onClick={handleSendMessage} id={`query-submit-${index}`} variant="light">
+                        <Search />
+                      </Button>
+                    </InputGroup.Text>
+                  </InputGroup>
+                )}
+              </React.Fragment>
+            )
           ))}
         </Col>
       </Row>

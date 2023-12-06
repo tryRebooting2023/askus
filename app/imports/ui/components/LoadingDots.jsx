2d9d8026d73205f111be97f8f32ae88cc9bbddfc
James Ligeralde <jligeral@hawaii.edu>
2023-12-06 09:28:09 -1000

    Changed dot size
    Small appears to be the smallest size setting

--- app/imports/ui/components/LoadingDots.jsx
+++ app/imports/ui/components/LoadingDots.jsx
@@ -4,9 +4,9 @@ import { Row, Spinner } from 'react-bootstrap';
 const LoadingDots = () => (
   <div className="spinner-container">
     <Row className="justify-content-md-center">
-      <Spinner animation="grow" variant="primary" className="custom-spinner" />
-      <Spinner animation="grow" variant="primary" className="custom-spinner" />
-      <Spinner animation="grow" variant="primary" className="custom-spinner" />
+      <Spinner animation="grow" size="sm" variant="primary" className="custom-spinner" />
+      <Spinner animation="grow" size="sm" variant="primary" className="custom-spinner" />
+      <Spinner animation="grow" size="sm" variant="primary" className="custom-spinner" />
     </Row>
   </div>
 );
